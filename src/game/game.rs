8cddd442aca89f9097f6b51d6a7d6ad070fcb796
Spheniscine <spheniscine@gmail.com>
2026-09-03 14:23:20 +0800

    gamestate

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,8 +1,13 @@
+use std::time::Duration;
+
 use enum_map::EnumMap;
 use enumset::EnumSet;
 use extend::ext;
+use rand::{Rng, seq::SliceRandom};
+use serde::{Deserialize, Serialize};
+use strum::IntoEnumIterator;
 
-use crate::game::{Board, DepotRole, Suit};
+use crate::game::{Board, BoardPos, Card, DECK_SIZE, DepotRole, RANKS, Skin, Suit};
 
 use super::AnimationAct;
 
@@ -79,5 +84,78 @@ impl Board {
         }
 
         counts
+    }
+}
+
+pub const ANIMATION_DURATION: Duration = Duration::from_millis(200);
+pub type AnimationKey = u16;
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub struct ActionRecord {
+    pos1: BoardPos, pos2: BoardPos,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
+pub enum ScreenState {
+    #[default] Game, 
+    Settings, Help,
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub struct GameState {
+    pub board: Board,
+    pub deal: Vec<Card>,
+    #[serde(skip)]
+    pub animation_key: AnimationKey, // used for syncing and to provide animator components with cycling keys
+    pub history: Vec<ActionRecord>,
+    pub undo_stack: Vec<usize>,
+    pub already_won: bool,
+    pub num_wins: i32,
+
+    pub screen_state: ScreenState,
+
+    pub allow_undo: bool,
+    pub skin: Skin,
+}
+
+impl GameState {
+    pub fn new_deal(rng: &mut impl Rng) -> Vec<Card> {
+        let mut deck = Vec::with_capacity(DECK_SIZE);
+        for rank in RANKS {
+            for suit in Suit::iter() {
+                deck.push(Card { rank, suit });
+            }
+        }
+
+        deck.shuffle(rng);
+        deck
+    }
+
+    pub fn init() -> Self {
+        let mut res = Self {
+            board: Board::empty(),
+            deal: vec![],
+            animation_key: 0,
+            history: vec![],
+            undo_stack: vec![],
+            already_won: false,
+            num_wins: 0,
+            screen_state: ScreenState::Game,
+            allow_undo: true,
+            skin: Skin::default(),
+        };
+
+        res.new_game();
+        res
+    }
+
+    pub fn new_game(&mut self) {
+        let deal = Self::new_deal(&mut rand::rng());
+        self.board = Board::from_deal(&deal);
+        self.deal = deal;
+        self.history.clear();
+        self.undo_stack.clear();
+        self.already_won = false;
+        // LocalStorage.save_game_state(&self);
     }
 }
